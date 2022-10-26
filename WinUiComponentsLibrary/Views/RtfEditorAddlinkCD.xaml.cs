e43668e74edbc69640efa2dec1ad8fb505ffc98f
ntxsay <39699697+ntxsay@users.noreply.github.com>
2022-10-26 09:39:44 -0400

    Mise à jour de l'application

--- WinUiComponentsLibrary/Views/RtfEditorAddlinkCD.xaml.cs
+++ WinUiComponentsLibrary/Views/RtfEditorAddlinkCD.xaml.cs
@@ -33,6 +33,9 @@ namespace WinUiComponentsLibrary.Views
         {
             this.InitializeComponent();
             if (!linkName.IsStringNullOrEmptyOrWhiteSpace())
+            {
+
+            }
 
         }
     }
