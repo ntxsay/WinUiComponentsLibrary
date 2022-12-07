904bd9b2841f416f4a4b2e5d8445d8ef10d4feeb
ntxsay <39699697+ntxsay@users.noreply.github.com>
2022-12-07 15:22:59 -0400

    Ajout de la fluent Window

--- WinUiComponentsLibrary/Code/Helpers/IBackdropHelpers.cs
+++ WinUiComponentsLibrary/Code/Helpers/IBackdropHelpers.cs
@@ -9,13 +9,31 @@ namespace WinUiComponentsLibrary.Code.Helpers
 {
     public interface IBackdropHelpers
     {
+        /// <summary>
+        /// Tente d'appliquer le thème Mica
+        /// </summary>
+        /// <returns>True si appliqué</returns>
         bool TrySetMicaBackdrop();
+
+        /// <summary>
+        /// Tente d'appliquer le thème Acrylique
+        /// </summary>
+        /// <returns>True si appliqué</returns>
         bool TrySetAcrylicBackdrop();
         void Window_Activated(object sender, WindowActivatedEventArgs args);
         void Window_Closed(object sender, WindowEventArgs args);
         void Window_ThemeChanged(FrameworkElement sender, object args);
         void SetConfigurationSourceTheme();
+
+        /// <summary>
+        /// Définit le type de thème à appliquer pour la fenêtre
+        /// </summary>
+        /// <param name="type">Type de thème</param>
         void SetBackdrop(BackdropType type);
+        
+        /// <summary>
+        /// Initialise la personnalisation du thème au démarrage de la fenêtre
+        /// </summary>
         void InitializeBackground();
     }
 }
--- WinUiComponentsLibrary/Code/Helpers/ICustomTitleBar.cs
+++ WinUiComponentsLibrary/Code/Helpers/ICustomTitleBar.cs
@@ -9,7 +9,15 @@ namespace WinUiComponentsLibrary.Code.Helpers
 {
     public interface ICustomTitleBar
     {
+        /// <summary>
+        /// Initialise la barre de titre personnalisée
+        /// </summary>
         public void InitializeAppTitleBar();
+
+        /// <summary>
+        /// Définit la zone draggable de la barre de titre
+        /// </summary>
+        /// <param name="appWindow">Fenêtre actuelle</param>
         public void SetDragRegionForCustomTitleBar(AppWindow appWindow);
     }
 }
