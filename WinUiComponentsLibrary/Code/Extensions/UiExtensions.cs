acf63f10266590e0164693f4a4907df536605dd8
ntxsay <39699697+ntxsay@users.noreply.github.com>
2022-12-12 09:14:28 -0400

    Mise à jour de l'application

---
+++ WinUiComponentsLibrary/Code/Extensions/UiExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Windows.Foundation;
+
+namespace WinUiComponentsLibrary.Code.Extensions
+{
+    public static class UiExtensions
+    {
+        public static bool ScrollToElement(this ScrollViewer scrollViewer, UIElement element, bool isVerticalScrolling = true, bool smoothScrolling = true, float? zoomFactor = null)
+        {
+            var transform = element.TransformToVisual((UIElement)scrollViewer.Content);
+            var position = transform.TransformPoint(new Point(0, 0));
+
+            if (isVerticalScrolling)
+            {
+                return scrollViewer.ChangeView(null, position.Y, zoomFactor, !smoothScrolling);
+            }
+            else
+            {
+                return scrollViewer.ChangeView(position.X, null, zoomFactor, !smoothScrolling);
+            }
+        }
+    }
+}
